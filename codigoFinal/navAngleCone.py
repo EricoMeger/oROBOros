43c426438d1bb89256bc8cf18b5d804ed9ebdc9c
Érico Meger <86668447+EricoMeger@users.noreply.github.com>
2022-12-03 14:39:22 -0300

    arrumando espaaços

--- codigoFinal/navAngleCone.py
+++ codigoFinal/navAngleCone.py
@@ -36,10 +36,12 @@ def getPosicao(gps):
     global latAtual
     global lonAtual
 
-    #O gpsd mostra as informações sendo recebidas pelo GPS em formato de lista, para que seja possível retirar informações dele é criada a varíavel px, que serve para passar para a próxima linha.
+    #O gpsd mostra as informações sendo recebidas pelo GPS em formato de lista, para que seja possível retirar informações dele é criada a varíavel px, 
+    #que serve para passar para a próxima linha.
     px = gpsd.next()
 
-    #Faz-se uma checagem se o gpsd retornou a classe 'TPV', significando que o GPS conseguiu um fix, ou seja, conseguiu fixar sua localização com três satélitese portanto está pronto para repassar as informações recebidas.
+    #Faz-se uma checagem se o gpsd retornou a classe 'TPV', significando que o GPS conseguiu um fix, ou seja, conseguiu fixar sua localização com três 
+    #satélites e portanto está pronto para repassar as informações recebidas.
     if px['class'] == 'TPV':
         #procura-se na lista strings de latitude e longitude, e retira a informações que elas estão repassando.
         latAtual = getattr(px, 'lat', "unknown")
@@ -67,7 +69,9 @@ def chamaBussola():
 
     global bussola
     
-    #Aqui se cria a instância do magnetômetro, definindo para ele sempre ficar ativo. Define-se também, no setDeclination a inclinação em graus e minutos da onde será executado o código, para que a bússola seja precisa. Será ocultado por motivos de segurança, mas você pode checar a inclinação do seu local  neste site: https://www.magnetic-declination.com/
+    #Aqui se cria a instância do magnetômetro, definindo para ele sempre ficar ativo. Define-se também, no setDeclination a inclinação em graus e 
+    #minutos da onde será executado o código, para que a bússola seja precisa. Será ocultado por motivos de segurança, mas você pode checar a inclinação 
+    #do seu local  neste site: https://www.magnetic-declination.com/
 
     bussola = i2c_hmc5883l.i2c_hmc5883l(1)
     bussola.setContinuousMode()
@@ -104,7 +108,8 @@ def segueCone(capture):
     minimum_area = 250
     maximum_area = 100000
 
-    #Foram tiradas duas cores do laranja: uma onde a luz do ambiente estava acesa e a outra com ela apagada, representada pelo LA e LD respectivamente. No momento está sendo usado somente a LA, mas é possível juntar os 4 ranges em uma máscara só e usá-la.
+    #Foram tiradas duas cores do laranja: uma onde a luz do ambiente estava acesa e a outra com ela apagada, representada pelo LA e LD respectivamente. 
+    #No momento está sendo usado somente a LA, mas é possível juntar os 4 ranges em uma máscara só e usá-la.
     lowerOrangeLA = np.array([0, 68, 255])
     upperOrangeLA = np.array([180, 255, 255])
 
@@ -131,7 +136,8 @@ while(x != 20):
 coneLonge = True
     x += 1
     print("ta indo")
-    #Esse while é necessário para a ativação do GPS, onde serve como uma forma de aguardar o fix ao mesmo tempo que retira as informações dele para que possam ser checadas na tela.
+    #Esse while é necessário para a ativação do GPS, onde serve como uma forma de aguardar o fix ao mesmo tempo que retira as informações dele para que 
+    #possam ser checadas na tela.
 
 coneLonge = True
 procurandoAngulo = True
@@ -142,7 +148,8 @@ getWaypoints()
 chamaBussola()
 calculaAngulo()
 
-# É definida uma aceitação de erro para que o robô encontre a coordenada da qual ele tem que ir, pensando tanto em uma possível demora para o robô parar, assim como um possível delay no magnetometro.
+# É definida uma aceitação de erro para que o robô encontre a coordenada da qual ele tem que ir, pensando tanto em uma possível demora para o robô parar, 
+#assim como um possível delay no magnetometro.
 
 minAngle = direcaoGraus - 3
 maxAngle = direcaoGraus + 3
@@ -162,7 +169,8 @@ while i != 3:
         areaContorno = cv2.contourArea(contours)
 
         if areaContorno > 1500:
-            #Caso a area contornada seja maior que 1500px, pega os dados da onde está sendo feito o contorno, para que se possa descobrir a onde o cone está em relação ao robô, esquerda, direita ou meio.   
+            #Caso a area contornada seja maior que 1500px, pega os dados da onde está sendo feito o contorno, para que se possa descobrir a onde o 
+            #cone está em relação ao robô, esquerda, direita ou meio.   
             x, y, width, height = cv2.boundingRect(contours)
             cv2.rectangle(capture, (x,y), (x + width, y + height), (0, 0, 255), 3)
             coneLonge = False
@@ -201,7 +209,8 @@ while i != 3:
                         escEsq.ChangeDutyCycle(7)
                         print("Seguindo em frente")
             
-            #Quando o cone está colado na câmera, a área do contorno fica em torno de 4800 a 5200. Como se faz necessário que o robô pare antes, é definido um range menor.
+            #Quando o cone está colado na câmera, a área do contorno fica em torno de 4800 a 5200. Como se faz necessário que o robô pare antes, 
+            #é definido um range menor.
             if areaContorno > 3800:
                 print("Cone muito perto")
                 escEsq.ChangeDutyCycle(0)
@@ -213,7 +222,8 @@ while i != 3:
     while coneLonge:
         while procurandoAngulo:
 
-            #Recebemos da bussola para onde o robô está indo, pegando o primeiro termo pois o magnetometro sempre retorna graus e minutos, mas aqui só importa os graus.
+            #Recebemos da bussola para onde o robô está indo, pegando o primeiro termo pois o magnetometro sempre retorna graus e minutos, 
+            #mas aqui só importa os graus.
             direcao = bussola.getHeading()
             anguloAtual = direcao[0]
     
@@ -221,7 +231,8 @@ while i != 3:
                 print("virando para a direita")
                 escEsq.ChangeDutyCycle(6.4)
                 escDir.ChangeDutyCycle(0)
-                #Não precisa que o angulo que o robô está agora seja exatamente o angulo que está a coordenada, portanto, colocamos-o dentro do range definido anteriormente.
+                #Não precisa que o angulo que o robô está agora seja exatamente o angulo que está a coordenada, portanto, colocamos-o dentro do range 
+                #definido anteriormente.
                 if (minAngle <= anguloAtual <= maxAngle):
                     procurandoAngulo = False
                     achouAngulo = True
